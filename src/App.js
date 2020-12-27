e985a0d64e15dc6557d07cf6824e2d7a21e62060
SergiAndreaUwU <sergiosalazarordaz@gmail.com>
2020-12-26 19:12:44 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const DATASAMPLE=[
+  {name: "Dan Abramov", avatar: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+]
+
+const Header= (props)=>{
+  return(
+    <div className="Header">
+    <h1>Welcome to {props.title}</h1>
     </div>
+  )
+}
+
+const Content=()=>{
+  return(
+    <div className="Content">
+    <Form/>
+    <CardList data={DATASAMPLE} />
+    </div>
+  )
+}
+
+class Form extends React.Component{
+   userInput= React.createRef();
+   handleSubmit = (event)=>{
+    event.preventDefault();
+    console.log(this.userInput.current.value)
+  }
+  render(){
+    return(
+      <div className="center">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="write something" ref={this.userInput} required/>
+          <button>Click me</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+const Card=(props)=>{
+
+  let profile=props;
+  return(
+  <div className="profile">
+    
+    <img src={profile.avatar}/>
+    <div className="profile-right">
+      <div className="name">{profile.name}</div>
+      <div className="company">{profile.company}</div>
+    </div>
+  </div>
+  )
+}
+
+const CardList= (props)=>{
+  return(
+    <>
+    {props.data.map((profile)=> <Card {...profile}/>)}
+    </>
+  )
+}
+
+const App= (props)=> {
+  return (
+    <>
+    <Header title={props.title} />
+    <Content/>
+    </>
   );
 }
 
