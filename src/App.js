f47ee2b7e97e2529ade84aee3ff499b66c01f6ea
SergiAndreaUwU <56396111+SergiAndreaUwU@users.noreply.github.com>
2021-01-24 21:38:12 -0600

    Add files via upload
    added test simulating click event

--- src/App.js
+++ src/App.js
@@ -1,120 +1,41 @@
-import './App.css';
-import React from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-const DATASAMPLE=[
-  {id:1 ,name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
-  {id:2 ,name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
-  {id:3 ,name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
-]
+function App() {
+  useEffect(() => {}, []);
 
-const Header= (props)=>{
-  return(
-    <div className="Header">
-    <h1>Welcome to {props.title}</h1>
-    </div>
-  )
-}
+  const [defaultCheck, setDefaultCheck] = useState(false);
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log("submitted!", e);
+  // };
 
-class Content extends React.Component{
-  render(){
-    return(
-      <ProfileCardsDemo/>
-    )
-  }
-}
+  const onClick = (e) => {
+    console.log(e);
+    console.log(defaultCheck);
 
-class ProfileCardsDemo extends React.Component{
-  state={ profiles: DATASAMPLE}
-  addNewProfile=(profileData)=>{
-    this.setState(prevState=>({
-      profiles: [...prevState.profiles, profileData]
-      })
-    )
-  }
-  render(){
-  return(
-    <div className="Content">
-    <Form onSubmit={this.addNewProfile}/>
-    <CardList data={this.state.profiles} />
-    </div>
-  )
-  }
-}
+    setDefaultCheck(!defaultCheck);
 
-class Form extends React.Component{
-  state={ userInput: ""}
-  handleSubmit = async (event)=>{
-    event.preventDefault();
-    const res= await getDataService(this.state)
-    this.props.onSubmit(res.data)
-    this.setState({userInput:""})
-  }
-  
-  render(){
-    return(
-      <div className="center">
-        <form onSubmit={this.handleSubmit}>
-          <input placeholder="write something"
-          value={this.state.userInput}
-          onChange={event=>{
-            this.setState({
-              userInput: event.target.value
-            })
-          }} 
-          required/>
-          <button>Click me</button>
-          <br/>
-          {/*insert an indicator which indicates if the user exists before clicking the button DB*/}
-        </form>
-      </div>
-    )
-  }
-}
+    console.log(defaultCheck);
+  };
 
-const getDataService=(state)=>{
-  try{
-  return axios.get(`https://api.github.com/users/${state.userInput}`)
-  }catch(e){
-    console.error(`error in getDataService: ${e}`)
-    try{
-      fetch(`https://api.github.com/users/${state.userInput}`)
-      .then(response => response.json())
-      .then(data => {return data});
-    }catch(e){console.error(e)}
-  }
-}
-
-const CardList= (props)=>{
-  return(
-    <>
-    {props.data.map((profile)=> <Card key={profile.id} {...profile}/>)} 
-    </>
-  )
-  //if you don't add a key property, it will throw an error about each child needing an unique key in a list
-}
-
-const Card=(props)=>{
-  let profile=props;
-  return(
-  <div className="profile">
-    <img src={profile.avatar_url} alt=""/>
-    <div className="profile-right">
-      <div className="name">{profile.name}</div>
-      <div className="company">{profile.company}</div>
-    </div>
-  </div>
-  )
-}
-
-
-
-const App= (props)=> {
   return (
-    <>
-    <Header title={props.title} />
-    <Content/>
-    </>
+    <div className="App">
+      {/* <form className="formularioxd" onSubmit={handleSubmit}>
+        <input className="inputxd" type="text"/>
+        <button className="buttonxd">submit button</button>
+        </form> */}
+      <div className="toggle-btn sm">
+        <input
+          id="1"
+          className="toggle-input round"
+          type="checkbox"
+          defaultChecked={defaultCheck}
+          onClick={(e) => onClick(e.target)}
+        />
+        <p style={{ color: "red" }}>{defaultCheck && <label>hola</label>}</p>
+      </div>
+    </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import Form from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+it.skip("pass", () => {
+  const wrapper = shallow(<Form />);
+
+  console.log(wrapper.debug());
+  expect(wrapper.find("form").length.valueOf(1));
+});
+
+it("will pass, triggers checkbox onChange event", () => {
+  const configs = {
+    default: true,
+    label: "My Label",
+    element: "myElement",
+  };
+ 
+  const checkbox = shallow(<Form configs={configs} />);
+  // checkbox.find("input").simulate("click");
+  const mockedEvent = { target: {} };
+
+  checkbox.find("input").simulate("click", mockedEvent);
+  checkbox.find("label").length.valueOf(1)
+
+  const errorLabel=checkbox.find("label")
+  errorLabel.text().valueOf("hola")
+
+});
+
+
+it("will fail without triggering checkbox onChange event", () => {
+  const configs = {
+    default: true,
+    label: "My Label",
+    element: "myElement",
+  };
+ 
+  const checkbox = shallow(<Form configs={configs} />);
+  checkbox.find("label").length.valueOf(1)
+
+  const errorLabel=checkbox.find("label")
+  errorLabel.text().valueOf("hola")
+
 });
